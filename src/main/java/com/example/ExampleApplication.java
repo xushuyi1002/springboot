bb8e0ed1081519b893c8ed365e00be1682b7fd89
xushuyi <xsy123456>
2019-05-08 14:43:16 +0800

    111

---
+++ src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: ExampleApplication
+ * @Description: ExampleApplication
+ * @author: Created by xushuyi <a href="xu_shuyi1002@163.com">Contact author</a>
+ * @date: 2019/5/8 14:36
+ * @Version: V1.0
+ */
+@SpringBootApplication
+public class ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
