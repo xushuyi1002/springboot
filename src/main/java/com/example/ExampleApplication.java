56a6962e44c97351c01dda222024ab0262d25415
xushuyi <xsy123456>
2019-05-09 10:31:08 +0800

    dev-111

--- src/main/java/com/example/ExampleApplication.java
+++ src/main/java/com/example/ExampleApplication.java
@@ -14,6 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
-        System.out.println("我是master分支...");
+        System.out.println("我是dev分支...");
     }
 }
